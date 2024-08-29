3c93b9c7e1241dfcaed229e6b6f5a4e82f1f2c73
withbenny <39987454+withbenny@users.noreply.github.com>
2024-08-29 21:05:22 +0800

    add long data

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# stock_simulator
+**This project is NOT finished yet!**
+
+## How to USE
+is NOT finished yet!
+## Data Explanation
+### 1. API
+
+You can see the example of [api data](examples\api.example.json) in ```/examples``` folder.
+
+The translation of its important part as follows:
+| | 中文 |
+| :--- | :--- |
+| "region"| 發行國 |
+| "quoteType" | 報價類型 |
+| "currency" | 貨幣 |
+| "bid" | 買價 |
+| "ask" | 賣價 |
+| "regularMarketOpen" | 開盤價 |
+| "averageDailyVolume3Month" | 三個月平均日交易量 |
+| "tradeable" | 交易狀態 |
+| "regularMarketTime" | 市場時間 |
+| "regularMarketDayHigh/Low" | 今日最高/低價 |
+| "regularMarketVolume" | 今日交易量 |
+| "regularMarketPreviousClose" | 前日收盤價 |
+| "regularMarketPrice" | 現在價格 |
+| "marketState" | 市場狀態 |
+| "regularMarketChangePercent" | 市場變化 |
+| "symbol" | 代碼 |
+
+
+## Standard of I/O
+![](images/flow.drawio_1.png)
+### 1. Financial News Standard
+| | type | 中文 |
+| :--- | :--- | :--- |
+| Title | / | 標題 |
+| Content | / | 內文 |
+| --- | --- | --- | 
+| Date | date | 日期 |
+| Source | str | 來源 |
+| Symbol/Industry | str | 股票/行業 |
+| Class | str | 類型 |
+| Weight | .2f | 權重 |
+
+### 2. Simulator Standard
+For Files:
+| | type |
+| :--- | :--- |
+| Stock Data from Stock API | json -> dict | 
+| Trade Data from USER | csv |
+| History Data^ | csv |
+
+NOTE: ^ History Data can be download from [Yahoo Finanace HK](https://hk.finance.yahoo.com/quote/AAPL/history?p=AAPL), but only for DAILY DATA. 
+
+---
+
+For Stock API:
+
+is NOT finished yet!
+
+
+For USER Trade data:
+| | type | 中文 |
+| :--- | :--- | :--- |
+| username^^ | str | 用戶名 |
+| symbol | str | 股票代碼 |
+| quantity | .1f | 持有量 |
+
+NOTE: ^^ Username is filename, follow the rule as below:
+1. Username starts with alphabetic character (either lowercase and upper case).
+2. Username can ONLY uses alphabetic characters and digits.
+3. In symbol, username + CASH is the user's CASH.
+
+---
+
+For History Data Infomation:
+| | type | 中文 |
+| :--- | :--- | :--- |
+| Date | YYYY-MM-DD(date) | 年-月-日 |
+| Time Range | Trading Days(int) | 總交易日天數 | 
+| Symbol | str | 股票代碼 |
+| Currency | str | 貨幣 |
+| Rate | data per day(/hour/minute) | 每天(/時/分)資料 |
+| Industry Type (GICS)^^^ | e.g. 15104050 (Steel) | 行業類型 |
+
+NOTE: ^^^ Industry types use the Sub-Industry of [GICS](https://en.wikipedia.org/wiki/Global_Industry_Classification_Standard).
+
+For Yahoo History Data CSV:
+| | type | 中文 |
+| :--- | :--- | :--- |
+| Date | date | 日期 |
+| Open | .2f | 開盤價 |
+| High | .2f | 最高價 |
+| Low | .2f | 最低價 |
+| Close | .2f | 收盤價 |
+| AdjClose | .5f | 調整收盤價 | 
+| Volume | int | 交易量 |
---
+++ stock.py
@@ -0,0 +1,232 @@
+import re
+import csv
+from datetime import datetime
+import json
+import math
+import os
+import re
+import time
+from collections import namedtuple
+from typing import Tuple
+
+import requests
+
+INIT_MONEY = 100_000
+
+class ConfigLoader:
+    def __init__(self, config_path:str, source:str) -> None:
+        self.config_path = config_path
+        self.source = source
+        self.config = self._load_config()
+    
+    def _load_config(self) -> dict:
+        with open(self.config_path, 'r') as f:
+            config = json.load(f)
+        
+        return config
+    
+    def get_api_key(self) -> str:
+        source = self.source
+        return self.config[source]['api_key']
+    
+    def get_api_url(self) -> str:
+        source = self.source
+        return self.config[source]['api_url']
+
+class GetTicker:
+    def __init__(self, symbol:str, config_loader:ConfigLoader) -> None:
+        self.symbol = symbol
+        self.config_loader = config_loader
+        self.api_url = self.config_loader.get_api_url()
+        self.api_key = self.config_loader.get_api_key()
+        self.current_year = datetime.now().year
+        self.current_month = datetime.now().month
+    
+    def latestData(self, date=None, interval:str='5min') -> dict:
+        if self.config_loader.source == 'alphavantage':
+            if date is None:
+                url = f"{self.api_url}query?function=TIME_SERIES_INTRADAY&symbol={self.symbol}&interval={interval}&outputsize=full&apikey={self.api_key}&datatype=csv"
+                response = requests.get(url)
+                data = response.text
+                time_now = datetime.now()
+                time_str = time_now.strftime("%Y-%m-%d_%H-%M-%S")
+            
+            else:
+                if not re.match(r'\d{4}-\d{2}', date) :
+                    raise ValueError("Invalid date format.")
+                
+                year, month = int(date.split('-')[0]), int(date.split('-')[1])
+                if year < 2000 or month < 1 or month > 12:
+                    raise ValueError("Invalid date. The API can only get data after 2000-01.")
+                elif year > self.current_year or (year == self.current_year and month > self.current_month):
+                    raise ValueError("You cannot get future data. Current date is " + str(self.current_year) + '-' + str(self.current_month) + '.')
+                
+                url = f"{self.api_url}query?function=TIME_SERIES_INTRADAY&symbol={self.symbol}&interval={interval}&month={date}&outputsize=full&apikey={self.api_key}&datatype=csv"
+                response = requests.get(url)
+                data = response.text
+                time_str = date
+
+            file_path = f'{self.symbol}_{time_str}.csv'
+            with open(file_path, 'w') as f:
+                f.write(data)
+
+            with open(file_path, 'r') as f:
+                reader = csv.reader(f)
+                data = list(reader)
+
+            data[0].append('unix_timestamp')
+            for row in data[1:]:
+                if len(row) < 1:
+                    continue
+                dt = datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S')
+                unix_timestamp = int(time.mktime(dt.timetuple()))
+                row.append(unix_timestamp)
+
+            with open(file_path, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+
+    def longData(self, start_date=None, end_date=None, interval:str='5min') -> dict:
+        if self.config_loader.source == 'alphavantage':
+            if start_date is None:
+                start_date = f'{self.current_year}-01'
+            if end_date is None:
+                end_date = f'{self.current_year}-{self.current_month}'
+
+            if not re.match(r'\d{4}-\d{2}', start_date) or not re.match(r'\d{4}-\d{2}', end_date):
+                raise ValueError("Invalid date format.")
+            
+            start_year, start_month = int(start_date.split('-')[0]), int(start_date.split('-')[1])
+            end_year, end_month = int(end_date.split('-')[0]), int(end_date.split('-')[1])
+
+            for year in range(start_year, end_year+1):
+                for month in range(1, 13):
+                    if (year == start_year and month < start_month) or (year == end_year and month > end_month):
+                        continue
+                    elif year == self.current_year and month > self.current_month:
+                        break
+
+                    date = f'{year}-{month:02}'
+                    self.latestData(date, interval)
+
+
+class Simulate:
+    # Data is stored in username.csv
+    def __init__(self, username: str, config_loader: ConfigLoader) -> None:
+        self.username = username
+        self.holdings = {}
+        self.config_loader = config_loader
+
+        # username can only be numbers[0-9] and letters[a-z, A-Z]
+        # username cannot start with a number
+        # username must be longer than 5
+        if not self.isValidName(username):
+            raise ValueError("Invalid username.")
+
+        # If username is new, create a new file
+        if not os.path.exists(username + '.csv'):
+            with open(username + '.csv', 'w', newline='') as csvfile:
+                headers = ['symbol', 'quantity']
+                writer = csv.DictWriter(csvfile, fieldnames=headers)
+                writer.writeheader()
+                writer.writerow({'symbol': username + 'CASH', 'quantity': INIT_MONEY})
+
+        # If username exists, load the data
+        else:
+            with open(username + '.csv', 'r', newline='') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    symbol = row['symbol']
+                    quantity = row['quantity']
+                    self.holdings[symbol] = quantity
+
+                if self.username + 'CASH' in self.holdings:
+                    self.cash = self.holdings.get(self.username + 'CASH')
+                else:
+                    self.cash = INIT_MONEY
+                    with open(username + '.csv', 'w', newline='') as csvfile:
+                        headers = ['symbol', 'quantity']
+                        writer = csv.DictWriter(csvfile, fieldnames=headers)
+                        writer.writeheader()
+                        writer.writerow({'symbol': username + 'CASH', 'quantity': self.cash})
+        print(str(self.holdings))
+
+    def isValidName(self, username: str) -> bool:
+        pattern = r'^[a-zA-Z][a-zA-Z0-9]*$'
+        if re.match(pattern, username):
+            if len(username) >= 5:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def currentCash(self) -> float:
+        return self.cash
+
+    def trade(self, symbol: str, mode: str, share: float) -> None:
+        if share <= 0:
+            raise ValueError("Invalid values.")
+
+        self.cash = self.holdings.get(self.username + 'CASH')
+        cash = self.cash
+        cash = float(cash)
+
+        # share keeps four decimal places to support fractional shares
+        share = math.floor(share * 10000) / 10000
+
+        # BUY
+        # Ignore the case of 'buy' and 'sell'
+        if mode.casefold() == 'buy':
+            price, _, _ = GetTicker(symbol, self.config_loader).latestPrice(GetTicker(symbol, self.config_loader).latestData())
+            cost = price * share
+            if cash - cost >= 0:
+                cash = cash - cost
+                # Change the current cash
+                self.holdings[self.username + 'CASH'] = cash
+
+                if symbol in self.holdings:
+                    self.holdings[symbol] = math.floor((float(self.holdings.get(symbol)) + share) * 10000) / 10000
+                else:
+                    self.holdings[symbol] = math.floor(share * 10000) / 10000
+            else:
+                raise ValueError("Invalid action.")
+
+        # SELL    
+        elif mode.casefold() == 'sell':
+            price, _, _ = GetTicker(symbol, self.config_loader).latestPrice(GetTicker(symbol, self.config_loader).latestData())
+            if symbol in self.holdings:
+                before = self.holdings.get(symbol)
+                if share <= before:
+                    income = price * share
+                    cash = cash + income
+                    # Change the current cash
+                    self.holdings[self.username + 'CASH'] = cash
+                    if share == before:
+                        del self.holdings[symbol]
+                    else:
+                        self.holdings[symbol] = math.floor((before - share) * 10000) / 10000
+                else:
+                    raise ValueError("Invalid action.")
+            else:
+                raise ValueError("Invalid action.")
+
+        # Other MODE
+        else:
+            raise ValueError("Invalid trade mode.")
+
+        print(str(self.holdings))
+        self.saveData()
+
+    def saveData(self) -> None:
+        # Save the data
+        with open(self.username + '.csv', 'w', newline='') as csvfile:
+            headers = ['symbol', 'quantity']
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            # Save the cash
+            writer.writerow({'symbol': self.username + 'CASH', 'quantity': self.holdings[self.username + 'CASH']})
+            # Save the holdings
+            for stock_symbol, quantity in self.holdings.items():
+                if stock_symbol != self.username + 'CASH':
+                    writer.writerow({'symbol': stock_symbol, 'quantity': quantity})
